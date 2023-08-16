afd16318560512092a08f552efada6e738fb3d05
Anastasia Lesnaya <lesnyasha@yandex.ru>
2023-08-17 01:04:24 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,20 +4,17 @@ const UnauthorizedError = require('../errors/Unauthorized');
 const { REQUIRE_AUTHORIZATION_MESSAGE } = require('../utils/constants');
 const { JWT_SECRET } = require('../utils/config');
 
-const auth = (req, res, next) => {
+const { NODE_ENV } = process.env;
+
+module.exports = (req, res, next) => {
   let payload;
   try {
-    const { authorization } = req.headers;
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-      return next(new UnauthorizedError(REQUIRE_AUTHORIZATION_MESSAGE));
-    }
-    const token = authorization.replace('Bearer ', '');
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (error) {
-    return next(new UnauthorizedError(REQUIRE_AUTHORIZATION_MESSAGE));
+    const token = req.cookies.jwt;
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : '🔐');
+  } catch (err) {
+    next(new UnauthorizedError(REQUIRE_AUTHORIZATION_MESSAGE));
+    return;
   }
-  req.userId = payload._id;
-  return next();
+  req.user = payload;
+  next();
 };
-
-module.exports = { auth };
