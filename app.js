a73d420d455d58b44472665e016da1a01c9da577
Anastasia Lesnaya <lesnyasha@yandex.ru>
2023-08-30 01:03:03 +0300

    cors

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 // Защита сервера
 const helmet = require('helmet');
 const { limiter } = require('./middlewares/limiter');
-
-const corsOptions = require('./middlewares/corsOptions');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, MONGODB } = require('./utils/config');
@@ -25,12 +24,24 @@ mongoose.connect(MONGODB, {
   useNewUrlParser: true,
 });
 
+const options = {
+  origin: [
+    'https://localhost:3000/',
+    'https://les.movies.nomoreparties.sbs/',
+  ],
+  allowedHeaders: ['Content-Туре', 'origin', 'Accept', 'Set-Cookie'],
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  preflightContinue: false,
+  credentials: true,
+};
+
+app.use('*', cors(options));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(requestLogger);
 app.use(helmet());
-app.use(corsOptions);
 
 app.use(limiter);
 
